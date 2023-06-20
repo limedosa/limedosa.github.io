28bb0293eec0a3d73fcbfd2a82d203e458655676
limedosa <109569567+limedosa@users.noreply.github.com>
2023-06-20 15:44:07 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import { createPopper } from "@popperjs/core";
+const popcorn = document.querySelector("#popcorn");
+const tooltip = document.querySelector("#tooltip");
+createPopper(popcorn, tooltip, {
+  placement: "top",
+});
